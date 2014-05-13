d0676e90e7b683afdc414f61edfd186ef897dd9a
wangrangzhi <451155928@qq.com>
2014-05-13 20:39:26 +0800

    new
    作业

--- 抽象类型/未命名5.cpp
+++ 抽象类型/未命名5.cpp
@@ -8,5 +8,8 @@ int main(){
 	char* p_chars = new char[100];
 	float* p_floats = new float[100];
 	
-	cout << ""
+	cout << "p_int == " << p_int << endl;
+	cout << "p_long == " << p_long << endl;
+	cout << "p_chars == " << static_cast<void*>(p_chars) << endl;
+	cout << "p_floats == " << p_floats << endl;
 }
